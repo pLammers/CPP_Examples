c367205e9046d3bfcf31ebd4dc786e3d1adc865d
pLammers <peter-lammers@hotmail.de>
2016-06-13 15:51:04 +0200

    inital calc

---
+++ dynamic_average_calculator/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <sstream>
+
+
+double average(std::vector<double> vec)
+{
+	double avg = 0;
+	for (int i = 0; i < vec.size(); i++)
+	{
+		avg += vec[i];
+	}
+	avg /= vec.size();
+
+	return avg;
+}
+
+int main()
+{
+	std::string line;
+	std::vector<double> input;
+	double value;
+	int count = 0;
+
+	std::cout << "Bitte Zahlen eingeben, Leerzeichen zur Trennung! '.' fuer Dezimalzahlen!" << std::endl;
+
+	
+	std::getline(std::cin, line);
+
+	std::istringstream iss(line);
+	while (iss >> value) {
+		input.push_back(value);
+	}
+
+	std::cout << "Der Durchschnitt betraegt: ";
+	std::cout << average(input) << std::endl;
+
+	std::cout << "Enter zum Beenden druecken...";
+	std::cin.get();
+	//system("pause");
+	return 0;
+}
